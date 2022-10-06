43021198d13dc816cdf008457d5fb6193255df24
simon <simoncabrera.sc@gmail.com>
2022-10-05 21:00:42 -0300

    primera entrega desafio final

--- src/components/productContainer/ProductListContainer.js
+++ src/components/productContainer/ProductListContainer.js
@@ -1,41 +1,40 @@
-import React, {useState, useEffect} from "react";
-import {getAllProducts, getProductByCategory} from '../productList/ProductList'
-import ProductCard from '../productCard/ProductCard';
+import React, { useState, useEffect } from "react";
+import {
+  getAllProducts,
+  getProductByCategory,
+} from "../productList/ProductList";
+import ProductCard from "../productCard/ProductCard";
 import { useParams } from "react-router-dom";
 
 function ProductListContainer() {
+  const [productList, setProductList] = useState([]);
+  const { cat } = useParams();
 
-    const [productList, setProductList] = useState([]);
-    const { cat } = useParams();
+  useEffect(() => {
+    if (cat === undefined) {
+      getAllProducts().then((result) => setProductList(result));
+    } else {
+      getProductByCategory(cat).then((result) => setProductList(result));
+    }
+  }, [cat]);
 
-    useEffect(()=>{
-      if (cat === undefined) {
-        getAllProducts().then((result) => setProductList(result));
-      }
-      else {
-        getProductByCategory(cat).then((result) => setProductList(result))
-      }
-    },[cat])
-   
-    
+  return (
+    <div className="product-container">
+      {productList.map((p) => {
+        return (
+          <ProductCard
+            key={p.id}
+            price={p.price}
+            stock={p.stock}
+            category={p.category}
+            img={p.img}
+            title={p.title}
+            id={p.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
 
-    return (
-      <div className="product-container">
-        {productList.map((p) => {
-          return (
-            <ProductCard
-              key={p.id}
-              price={p.price}
-              stock={p.stock}
-              category={p.category}
-              img={p.img}
-              title={p.title}
-              id={p.id}
-            />
-          );
-        })}
-      </div>
-    );
-  };
-  
-export default ProductListContainer
+export default ProductListContainer;
